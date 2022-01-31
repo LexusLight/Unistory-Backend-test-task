75cb3c425ce3de86322d1d6dc87238800a6b8cca
Konstantin Lexov <kostyaelizarov@mail.ru>
2022-01-31 20:04:51 +0300

    Revert "Revert "Project Inti""
    This reverts commit a4bb79cd9a219d6876390c563cd79aa703ef389c.

---
+++ project/src/article/articles.ts
@@ -0,0 +1,6 @@
+import {ArticleModel} from  "./article.model";
+
+export const articles: ArticleModel[] = [
+  new ArticleModel("The first one", "Lorem ipsum ...", 1),
+  new ArticleModel("The second one", "Lorem ipsum ...", 2),
+]
